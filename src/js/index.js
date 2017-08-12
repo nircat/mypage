460078114d9c17a87b5ce8310ddadc43b1f0ff24
yir <app7th@gmail.com>
2017-08-12 10:26:41 +0900

    [update] hover sound2

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 /**
- * Created by You on 2017/08/12.
+ * Created by yir on 2017/08/12.
  */
+
+//onLoadEvent
+window.onload = function () {
+    var link = document.querySelectorAll(".link");
+    for (var i=0;  i<link.length; i++) {
+        onMouseSoundPlay(link[i]);
+    }
+};
+//elemにホバー時効果音のイベントを追加
+function onMouseSoundPlay(elem) {
+    var audio = document.querySelector("#hovSound");
+    elem.onmouseover = function () {
+        audio.currentTime = 0;
+        audio.play();
+    }
+}
